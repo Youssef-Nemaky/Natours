3c35a50704036001a27901a41fe8be288afb968f
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2022-11-29 03:06:57 +0200

    Add tour controllers

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,21 +1,67 @@
+const Tour = require('../models/tourModel');
+
 const getAllTours = async (req, res) => {
-  res.status(200).send('Get All Tours');
+  try {
+    const tours = await Tour.find({});
+    res.status(200).json({ length: tours.length, tours });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const getTour = async (req, res) => {
-  res.status(200).send('Get Tour');
+  try {
+    const tour = await Tour.findById(req.params.id);
+    if (!tour) {
+      return res
+        .status(404)
+        .json({ Error: `Tour with id: ${req.params.id} was not found` });
+    }
+    res.status(200).json(tour);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const updateTour = async (req, res) => {
-  res.status(200).send('Update Tour');
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    if (!tour) {
+      return res
+        .status(404)
+        .json({ Error: `Tour with id: ${req.params.id} was not found` });
+    }
+
+    res.status(200).json(tour);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const createTour = async (req, res) => {
-  res.status(200).send('Create Tour');
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json(newTour);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const deleteTour = async (req, res) => {
-  res.status(200).send('Delete Tour');
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    if (!tour) {
+      return res
+        .status(404)
+        .json({ Error: `Tour with id: ${req.params.id} was not found` });
+    }
+    res.status(200).json(tour);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
